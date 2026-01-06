fa5380b24d61e50de58fc445077a9dea50970665
nerikeb <nerike.b@gmail.com>
2026-01-06 01:19:01 +0100

    Added fix to make insulin show up in recent logs

--- lib/models/log_entry_dto.dart
+++ lib/models/log_entry_dto.dart
@@ -2,16 +2,22 @@ class LogEntryDTO {
   final String type; // "Food" or "Insulin"
   final DateTime timestamp;
   final String description;
+  final String? carbs;
+  final String? calories;
 
   LogEntryDTO({
     required this.type,
     required this.timestamp,
     required this.description,
+    this.carbs,
+    this.calories,
   });factory LogEntryDTO.fromJson(Map<String, dynamic> json) {
     return LogEntryDTO(
       type: json['type'] ?? 'Unknown',
       timestamp: DateTime.parse(json['timestamp']),
       description: json['description'] ?? '',
+      carbs: json['carbs'],
+      calories: json['calories'],
     );
   }
 }
